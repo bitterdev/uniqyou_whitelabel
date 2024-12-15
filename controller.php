7b353ff814fd2309658745bd749d593b8710a14b
Fabian Bitter <fabian@bitter.de>
2024-12-15 22:07:07 +0100

    Update

--- controller.php
+++ controller.php
@@ -2,6 +2,7 @@
 
 namespace Concrete\Package\UniqyouWhitelabel;
 
+use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Package\Package;
 
 class Controller extends Package
@@ -18,6 +19,16 @@ class Controller extends Package
     public function getPackageName(): string
     {
         return t('uniqyou whitelabel');
+    }
+
+    public function on_start()
+    {
+        /** @var Repository $config */
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $config = $this->app->make(Repository::class);
+
+        $config->set("concrete.white_label.logo", $this->getRelativePath() . "/images/logo.svg");
+        $config->set("concrete.white_label.name", "uniqyou CMS");
     }
 
 }
